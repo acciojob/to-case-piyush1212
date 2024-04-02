6d075fc3b014608450d8815bab4d688d7ec29913
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-04-02 20:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toCase(text) {
-  // write your code here
+    if (!text) {
+        return '-';
+    }
+    
+    const lower = text.toLowerCase();
+    const upper = text.toUpperCase();
+    
+    return `${lower}-${upper}`;
 }
-
-// DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
